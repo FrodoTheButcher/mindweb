53fe3488d4a9ce89fde1180ceb9f96e44c3bd155
unknown <robifodor69@yahoo.com>
2023-04-12 00:34:16 -0700

    changes

--- myportofolio/portofolio/views.py
+++ myportofolio/portofolio/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
+import random
+import string
 
 # Create your views here.
 def main(request):
+    if request.method=="POST":
+        message = request.POST['message']
+        name = request.POST['name']
+        contact =request.POST['contact']
+        
+        send_mail(
+               "Cererea mesaj de catre " + name,
+                        "pentru "+ message + " venita de la "+ name,
+                        settings.EMAIL_HOST_USER,
+                            [ settings.EMAIL_HOST_USER],
+                
+                            fail_silently=False,
+                        )
     return render(request,"main.html")
